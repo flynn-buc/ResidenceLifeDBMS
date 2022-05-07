c99bc27c0773689d1b3f830aef10d77c8c967d0b
Jonathan H <54959558+jmhirsch@users.noreply.github.com>
2022-05-07 03:23:45 -0700

    Delete Examples/Join directory

--- Examples/Join/3. Query.sql
+++
@@ -1,5 +0,0 @@
-SELECT HOUSENAME, COUNT(*) as numOfResidents, round(AVG(round(months_between(sysdate,to_date(dob,'MM-DD-YYYY'))/12)), 2) as AvgAge, round(MAX(round(months_between(sysdate,to_date(dob,'MM-DD-YYYY'))/12)), 2) as Oldest 
-FROM RESIDENTINFO NATURAL JOIN RESIDENTADDRESS NATURAL JOIN HOUSE 
-WHERE (round(months_between(sysdate,to_date(dob,'MM-DD-YYYY'))/12)) > 1 AND
-		dob IS NOT NULL 
-GROUP BY HOUSENAME
